69770451ce3e29086ab7e89a754a7d7eea457dc4
QQQtx <1102214883@qq.com>
2024-01-29 17:14:16 +0800

    feat:测试方法

---
+++ src/main/java/qtx/spring/flowable/config/SendRejectionMail.java
@@ -0,0 +1,15 @@
+package qtx.spring.flowable.config;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+
+/**
+ * @author qtx
+ * @since 2024/1/29
+ */
+public class SendRejectionMail implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) {
+
+    }
+}
--- src/test/java/qtx/spring/flowable/FlowableApplicationTests.java
+++ src/test/java/qtx/spring/flowable/FlowableApplicationTests.java
@@ -1,16 +1,21 @@
 package qtx.spring.flowable;
 
 import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.RepositoryService;
 import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@SpringBootTest(classes = FlowableApplication.class, properties = {"vm_ip=192.168.77.130"})
+@SpringBootTest(classes = FlowableApplication.class, properties = "vm_ip=192.168.77.130")
 class FlowableApplicationTests {
 
     @Autowired
@@ -21,19 +26,88 @@ class FlowableApplicationTests {
         System.out.println("~~~~~~~~~");
     }
 
+    /**
+     * 手动部署流程
+     */
+    @Test
+    void testDeploy() {
+        // 部署流程 获取RepositoryService对象
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        // 创建Deployment对象
+        Deployment deployment = repositoryService.createDeployment()
+                // 添加流程部署文件
+                .addClasspathResource("processes/公司员工请假流程.bpmn20.xml")
+                // 设置部署流程的名称
+                .name("公司员工请假流程测试")
+                // 执行部署操作
+                .deploy();
+        System.out.println("deployment.getId() = " + deployment.getId());
+        System.out.println("deployment.getName() = " + deployment.getName());
+
+    }
+
+    /**
+     * 运行流程
+     */
     @Test
     void testRunProcess() {
         RuntimeService runtimeService = processEngine.getRuntimeService();
         // 构建流程变量
         Map<String, Object> variables = new HashMap<>();
-        variables.put("employee", "张三");// 谁申请请假
-        variables.put("day", 3); // 请几天假
-        variables.put("description", "工作累了，想出去玩玩"); // 请假的原因
+        // 谁申请请假
+        variables.put("studentUser", "张三");
+        // 请几天假
+        variables.put("day", 3);
+        // 请假的原因
+        variables.put("description", "工作累了，想出去玩玩");
         ProcessInstance leave = runtimeService.startProcessInstanceByKey("leave", variables);
         // 输出相关的流程实例信息
         System.out.println("流程定义的ID：" + leave.getProcessDefinitionId());
         System.out.println("流程实例的ID：" + leave.getId());
         System.out.println("当前活动的ID：" + leave.getActivityId());
+    }
+
+    @Test
+    void testTaskUser() {
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> taskList = taskService.createTaskQuery()
+                .processDefinitionKey("leave")
+                .taskAssignee("张三")
+                .list();
+        System.out.println("任务数量：" + taskList.size());
+        taskList.forEach(task -> {
+            System.out.println("任务名称：" + task.getName());
+            System.out.println("任务ID：" + task.getId());
+            System.out.println("任务的创建时间：" + task.getCreateTime());
+            System.out.println("任务的优先级：" + task.getPriority());
+        });
+    }
+
+    /**
+     * 查询任务组
+     */
+    @Test
+    void testTaskGroup() {
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> taskList = taskService.createTaskQuery()
+                .processDefinitionKey("leave")
+                .list();
+        System.out.println("任务数量：" + taskList.size());
+        taskList.forEach(task -> {
+            System.out.println("任务名称：" + task.getName());
+            System.out.println("任务ID：" + task.getId());
+            System.out.println("任务的创建时间：" + task.getCreateTime());
+        });
+    }
+
+    @Test
+    void testCompleteTask() {
+        TaskService taskService = processEngine.getTaskService();
+        // 添加流程变量
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("outcome", "通过"); // 拒绝请假
+        // 完成任务
+        taskService.complete("ba0a3ffc-be57-11ee-8071-60e9aa3ca244", variables);
     }
 
 }
