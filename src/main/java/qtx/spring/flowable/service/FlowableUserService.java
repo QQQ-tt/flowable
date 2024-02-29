6eb70795b3a9627007841a35aae34888cbac298a
QQQtx <1102214883@qq.com>
2024-02-29 11:10:56 +0800

    feat:用户管理

--- src/main/java/qtx/spring/flowable/service/FlowableUserService.java
+++ src/main/java/qtx/spring/flowable/service/FlowableUserService.java
@@ -1,8 +1,65 @@
 package qtx.spring.flowable.service;
 
+import org.flowable.idm.api.Group;
+import org.flowable.idm.api.User;
+
 /**
  * @author qtx
  * @since 2024/2/28
  */
 public interface FlowableUserService {
+
+    /**
+     * 创建用户
+     * @param userId 用户id
+     * @param firstName 用户名
+     * @param lastName 用户名
+     * @param email 邮箱
+     */
+    void createUser(String userId, String firstName, String lastName, String email);
+
+    /**
+     * 更新用户
+     * @param updatedUser 用户
+     */
+    void updateUser(User updatedUser);
+
+    /**
+     * 删除用户
+     * @param userId 用户id
+     */
+    void deleteUser(String userId);
+
+    /**
+     * 根据用户id查询用户
+     * @param userId 用户id
+     * @return 用户
+     */
+    User findUserById(String userId);
+
+    /**
+     * 创建用户组
+     * @param groupId 用户组id
+     * @param groupName 用户组名
+     */
+    void createGroup(String groupId, String groupName);
+
+    /**
+     * 更新用户组
+     * @param updatedGroup 用户组
+     */
+    void updateGroup(Group updatedGroup);
+
+    /**
+     * 删除用户组
+     * @param groupId 用户组id
+     */
+    void deleteGroup(String groupId);
+
+    /**
+     * 根据用户组id查询用户组
+     * @param groupId 用户组id
+     * @return 用户组
+     */
+    Group findGroupById(String groupId);
 }
--- src/main/java/qtx/spring/flowable/service/impl/FlowableUserServiceImpl.java
+++ src/main/java/qtx/spring/flowable/service/impl/FlowableUserServiceImpl.java
@@ -1,5 +1,7 @@
 package qtx.spring.flowable.service.impl;
 
+import org.flowable.idm.api.Group;
+import org.flowable.idm.api.User;
 import org.springframework.stereotype.Service;
 import qtx.spring.flowable.common.FlowableFactory;
 import qtx.spring.flowable.service.FlowableUserService;
@@ -10,4 +12,53 @@ import qtx.spring.flowable.service.FlowableUserService;
  */
 @Service
 public class FlowableUserServiceImpl extends FlowableFactory implements FlowableUserService {
+
+
+    @Override
+    public void createUser(String userId, String firstName, String lastName, String email) {
+        User user = getIdentityService().newUser(userId);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        getIdentityService().saveUser(user);
+    }
+
+    @Override
+    public void updateUser(User updatedUser) {
+        getIdentityService().saveUser(updatedUser);
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+        getIdentityService().deleteUser(userId);
+    }
+
+    @Override
+    public User findUserById(String userId) {
+        return getIdentityService().createUserQuery()
+                .userId(userId)
+                .singleResult();
+    }
+
+    @Override
+    public void createGroup(String groupId, String groupName) {
+        Group group = getIdentityService().newGroup(groupId);
+        group.setName(groupName);
+        getIdentityService().saveGroup(group);
+    }
+
+    @Override
+    public void updateGroup(Group updatedGroup) {
+        getIdentityService().saveGroup(updatedGroup);
+    }
+
+    @Override
+    public void deleteGroup(String groupId) {
+        getIdentityService().deleteGroup(groupId);
+    }
+
+    @Override
+    public Group findGroupById(String groupId) {
+        return getIdentityService().createGroupQuery().groupId(groupId).singleResult();
+    }
 }
